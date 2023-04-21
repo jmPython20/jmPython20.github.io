8390a37ca4437f6303c60387612fd78408b250e2
jmPython20 <122218584+jmPython20@users.noreply.github.com>
2023-04-21 12:27:01 +0200

    Add files via upload
    dodanie skryptu przewijania zdj

--- app.js
+++ app.js
@@ -45,3 +45,54 @@ function closeFullImg(){
     fullimgbox.style.display = "none";
     
 }
+let images = [];
+// Pobranie diva
+let myDiv = document.getElementsByClassName('grid_galery three-col-grid_galery');
+
+// Pobranie elementów img w divie
+let imgElements = document.getElementsByClassName('img_galery');
+
+// Dodanie elementów do tablicy
+for (let i = 0; i < imgElements.length; i++) {
+    let src = imgElements[i].getAttribute('src');
+    images.push(src);
+}
+for (let i = 0; i < images.length; i++) {
+    console.log(images[i]);
+}
+function openFullImg(pic){
+    fullimgbox.style.display = "flex";
+    fullimg.src= pic;
+    console.log(fullimg.src);
+}
+function closeFullImg(){
+    fullimgbox.style.display = "none"
+}
+
+
+function goRight(){
+    let element = fullimg.getAttribute('src');
+    console.log(element);
+    let position = images.indexOf(element.replace('http://127.0.0.1:5501/drewland_realizacje/',''));
+    const positionActual= position + 1;
+    const lastIndex = images.length - 1;
+    if (position !== -1 && positionActual <= lastIndex) {
+        console.log(`Pozycja elementu "${element}" w tablicy to: ${position}`);
+        fullimg.src= images[position+1];
+    } else {
+        fullimg.src= images[0];
+    }
+}
+function goLeft(){
+    let element = fullimg.getAttribute('src');
+    console.log(element);
+    let position = images.indexOf(element.replace('http://127.0.0.1:5501/drewland_realizacje/',''));
+    const positionActual= position - 1;
+    const lastIndex = images.length - 1;
+    if (position !== -1 && positionActual >=0) {
+        console.log(`Pozycja elementu "${element}" w tablicy to: ${position}`);
+        fullimg.src= images[position-1];
+    } else {
+        fullimg.src= images[lastIndex];
+    }
+}
